64a4d02b3e70ea358fb847c4c144ab3af3b6ab5c
songqiang <470472264@qq.com>
2018-04-27 11:54:28 +0800

    去空格

--- src/main/java/com/zzm/demo/common/DateConverter.java
+++ src/main/java/com/zzm/demo/common/DateConverter.java
@@ -7,7 +7,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
- * Created by sang on 2018/1/13.
+ * Created by songQiang on 2018/1/13.
  */
 public class DateConverter implements Converter<String,Date> {
     private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
